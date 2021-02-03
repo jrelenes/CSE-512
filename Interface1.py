29a10d2840b4f8c6fcea0f0d72cf58e18fce247f
jrelenes <56177974+jrelenes@users.noreply.github.com>
2021-02-03 16:34:34 -0700

    Add files via upload

--- Interface1.py
+++ Interface1.py
@@ -91,30 +91,29 @@ def rangeQuery(ratingMinValue, ratingMaxValue, openconnection, outputPath):
             openconnection.commit()
             sql = "COPY (WITH temp AS (SELECT table_name2, userid, movieid, rating FROM range_ratings_part"+str(i)+" WHERE rating >= "+str(ratingMinValue)+" AND rating <= "+str(ratingMaxValue)+" ) SELECT * from temp) TO STDOUT WITH CSV DELIMITER ','"
             cur.copy_expert(sql, file)
-            
+
 def pointQuery(ratingValue, openconnection, outputPath):
     global RROBIN_TABLE_PREFIX
     global RANGE_TABLE_PREFIX
     cur = openconnection.cursor()
-    #cur.execute('CREATE TABLE output_point_query (PartitionName varchar, UserID int, MovieID int, Rating int);')
-    #openconnection.commit()
 
-    for i in range(RROBIN_TABLE_PREFIX):
-        cur.execute(
-            'INSERT INTO output_point_query (UserID,MovieID, Rating ) SELECT * FROM round_robin_ratings_part' + str(i) + ';')
-        cur.execute('UPDATE output_point_query SET PartitionName=%s WHERE PartitionName IS NULL',
-                    ('round_robin_ratings_part' + str(i),))
-        openconnection.commit()
-    for i in range(RANGE_TABLE_PREFIX):
-        cur.execute('INSERT INTO output_point_query (UserID,MovieID, Rating ) SELECT * FROM range_ratings_part' + str(i) + ';')
-        cur.execute('UPDATE output_point_query SET PartitionName=%s WHERE PartitionName IS NULL', ('range_ratings_part' + str(i),))
-        openconnection.commit()
-
-    cur.execute('SELECT * FROM output_point_query WHERE Rating = ' + str(ratingValue))
-    sql = "COPY (SELECT * FROM output_point_query) TO STDOUT WITH CSV DELIMITER ','"
-    with open("/home/not-yours/Documents/Assignment1/table.txt", "w") as file:
-        cur.copy_expert(sql, file)
-
+    with open("/home/not-yours/Documents/Assignment1/table2.txt", "w") as file:
+        for i in range(RROBIN_TABLE_PREFIX):
+            name = "'round_robin_ratings_part" + str(i) + "'"
+            cur.execute(
+                'ALTER TABLE round_robin_ratings_part' + str(i) + ' ADD COLUMN table_name3 VARCHAR default ' + name)
+            openconnection.commit()
+            sql = "COPY (WITH temp AS (SELECT table_name3, userid, movieid, " \
+                  "rating FROM round_robin_ratings_part" + str(i) + ") SELECT * from temp WHERE rating =" + str(
+                ratingValue)+") TO STDOUT WITH CSV DELIMITER ','"
+            cur.copy_expert(sql, file)
+        for i in range(RANGE_TABLE_PREFIX):
+            name = "'range_ratings_part" + str(i) + "'"
+            cur.execute('ALTER TABLE range_ratings_part' + str(i) + ' ADD COLUMN table_name4 VARCHAR default ' + name)
+            openconnection.commit()
+            sql = "COPY (WITH temp AS (SELECT table_name4, userid, movieid, rating FROM range_ratings_part" + str(
+                i) + " WHERE rating = " + str(ratingValue)+") SELECT * from temp) TO STDOUT WITH CSV DELIMITER ','"
+            cur.copy_expert(sql, file)
 def createDB(dbname='dds_assignment1'):
     """
     We create a DB by connecting to the default user and database of Postgres
@@ -168,10 +167,10 @@ ratingsfilepath = '/home/not-yours/Documents/ml-10M100K/' + ratingstablename + '
 # conn = getOpenConnection()
 # cur = conn.cursor()
 #loadRatings(ratingstablename, ratingsfilepath, getOpenConnection())
-rangePartition(ratingstablename,6,getOpenConnection())
+#rangePartition(ratingstablename,6,getOpenConnection())
 # print(cur.execute("SELECT * FROM test;"))
 # print(cur.fetchall())
-roundRobinPartition(ratingstablename,6,getOpenConnection())
+#roundRobinPartition(ratingstablename,6,getOpenConnection())
 #roundRobinInsert(ratingstablename,1,364,1,getOpenConnection())
-rangeQuery(3,4,getOpenConnection(),'/home/not-yours/Documents/ml-10M100K/')
-#pointQuery(5,getOpenConnection(),'/home/not-yours/Documents/ml-10M100K/')
+#rangeQuery(3,4,getOpenConnection(),'/home/not-yours/Documents/ml-10M100K/')
+pointQuery(5,getOpenConnection(),'/home/not-yours/Documents/ml-10M100K/')
