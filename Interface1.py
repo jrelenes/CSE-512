1d9bdb271c10e6a33969fe49e1c852939331cecd
jrelenes <56177974+jrelenes@users.noreply.github.com>
2021-02-02 23:32:10 -0700

    Add files via upload

--- Interface1.py
+++ Interface1.py
@@ -1,7 +1,8 @@
 import psycopg2
 import os
 import sys
-
+RANGE_TABLE_PREFIX = 3
+RROBIN_TABLE_PREFIX = 3
 
 def getOpenConnection(user='postgres', password='1234', dbname='postgres'):
     return psycopg2.connect("dbname='" + dbname + "' user='" + user + "' host='localhost' password='" + password + "'")
@@ -22,15 +23,18 @@ def loadRatings(ratingstablename, ratingsfilepath, openconnection):
 def rangePartition(ratingstablename, numberofpartitions, openconnection):
      cur = openconnection.cursor()
      cur.execute('SELECT COUNT(*) FROM ' + ratingstablename)
+     global RANGE_TABLE_PREFIX
+     if RANGE_TABLE_PREFIX < numberofpartitions:
+        RANGE_TABLE_PREFIX = numberofpartitions
      p = cur.fetchall()[0][0]
      if p % numberofpartitions == 0:
          const = int(p / numberofpartitions)
          skip = 0
          last = p - const
          for i in range(numberofpartitions):
-          cur.execute('CREATE TABLE rangePartition'+str(i)+'(userid int, movieid int, rating float)')
+          cur.execute('CREATE TABLE range_ratings_part'+str(i)+'(userid int, movieid int, rating float)')
           openconnection.commit()
-          cur.execute('INSERT INTO rangePartition'+str(i) +' SELECT * FROM ' + ratingstablename+' OFFSET '+str(skip)+' LIMIT '+str(const)+';')
+          cur.execute('INSERT INTO range_ratings_part'+str(i) +' SELECT * FROM ' + ratingstablename+' ORDER BY rating ASC OFFSET '+str(skip)+' LIMIT '+str(const)+';')
           openconnection.commit()
           skip += const
      else:
@@ -38,43 +42,79 @@ def rangePartition(ratingstablename, numberofpartitions, openconnection):
 
 def roundRobinPartition(ratingstablename, numberofpartitions, openconnection):
      cur = openconnection.cursor()
-
+     global RROBIN_TABLE_PREFIX
+     if RROBIN_TABLE_PREFIX < numberofpartitions:
+        RROBIN_TABLE_PREFIX = numberofpartitions
      cur.execute('SELECT COUNT(*) FROM '+str(ratingstablename))
      i = cur.fetchall()[0][0]
      skip = 0
      for k in range(numberofpartitions):
-            cur.execute('CREATE TABLE round_robin'+str(k)+'(userid int, movieid int, rating float)')
+            cur.execute('CREATE TABLE round_robin_ratings_part'+str(k)+'(userid int, movieid int, rating float)')
             openconnection.commit()
      while i > 0:
         for j in range(numberofpartitions):
-            cur.execute('INSERT INTO round_robin'+str(j)+' SELECT * FROM ' + ratingstablename+ ' LIMIT 1 OFFSET '+str(skip))
+            cur.execute('INSERT INTO round_robin_ratings_part'+str(j)+' SELECT * FROM ' + ratingstablename+ ' ORDER BY rating ASC LIMIT 1 OFFSET '+str(skip))
+
             skip += 1
             openconnection.commit()
         i -= numberofpartitions
 
-    # cur.execute('WITH round_robin_total(
-    # for k in numberofpartitions:
-    #	SELECT * FROM ' + round_robin+str(k) +';')
-    #pass  # Remove this once you are done with implementation
-
-
 def roundRobinInsert(ratingstablename, userid, itemid, rating, openconnection):
-    # cur = openconnection.cursor
-    # cur.execute('INSERT INTO '+ ratingstablename+'(*) 'SELECT * FROM '+ round_robin_total)
-    pass  # Remove this once you are done with implementation
+     cur = openconnection.cursor
+     cur.execute(''' select count(*) from information_schema.tables where tables.table_name LIKE '%round_robin_ratings_part%'; ''')
+     #print(cur.fetchall())
+     #cur.execute('INSERT INTO '+ ratingstablename+ ' VALUES (%d,%d,%f), ('+userid+','+ itemid+','+rating+');')
 
 
 def rangeInsert(ratingstablename, userid, itemid, rating, openconnection):
+    #cur = openconnection.cursor()
+    #cur.execute("SELECT * FROM pg_catalog.pg_tables")
     pass  # Remove this once you are done with implementation
 
 
 def rangeQuery(ratingMinValue, ratingMaxValue, openconnection, outputPath):
-    pass  # Remove this once you are done with implementation
+    global RROBIN_TABLE_PREFIX
+    global RANGE_TABLE_PREFIX
+    cur = openconnection.cursor()
+    #cur.execute('CREATE TABLE output (PartitionName varchar, UserID int, MovieID int, Rating int);')
+    #openconnection.commit()
 
+    for i in range(RROBIN_TABLE_PREFIX):
+        cur.execute('INSERT INTO output (UserID,MovieID, Rating ) SELECT * FROM round_robin_ratings_part'+str(i)+';')
+        cur.execute('UPDATE output SET PartitionName=%s WHERE PartitionName IS NULL', ('round_robin_ratings_part'+str(i),))
+        openconnection.commit()
+    for i in range(RANGE_TABLE_PREFIX):
+        cur.execute('INSERT INTO output (UserID,MovieID, Rating ) SELECT * FROM range_ratings_part'+str(i)+';')
+        cur.execute('UPDATE output SET PartitionName=%s WHERE PartitionName IS NULL', ('range_ratings_part'+str(i),))
+        openconnection.commit()
+
+    cur.execute('SELECT * FROM output WHERE Rating >= '+str(ratingMinValue)+' AND Rating <= '+str(ratingMaxValue))
+    sql = "COPY (SELECT * FROM output_point_query) TO STDOUT WITH CSV DELIMITER ','"
+    with open("/home/not-yours/Documents/Assignment1/table.txt", "w") as file:
+        cur.copy_expert(sql, file)
 
 def pointQuery(ratingValue, openconnection, outputPath):
-    pass  # Remove this once you are done with implementation
+    global RROBIN_TABLE_PREFIX
+    global RANGE_TABLE_PREFIX
+    cur = openconnection.cursor()
+    #cur.execute('CREATE TABLE output_point_query (PartitionName varchar, UserID int, MovieID int, Rating int);')
+    #openconnection.commit()
 
+    for i in range(RROBIN_TABLE_PREFIX):
+        cur.execute(
+            'INSERT INTO output_point_query (UserID,MovieID, Rating ) SELECT * FROM round_robin_ratings_part' + str(i) + ';')
+        cur.execute('UPDATE output_point_query SET PartitionName=%s WHERE PartitionName IS NULL',
+                    ('round_robin_ratings_part' + str(i),))
+        openconnection.commit()
+    for i in range(RANGE_TABLE_PREFIX):
+        cur.execute('INSERT INTO output_point_query (UserID,MovieID, Rating ) SELECT * FROM range_ratings_part' + str(i) + ';')
+        cur.execute('UPDATE output_point_query SET PartitionName=%s WHERE PartitionName IS NULL', ('range_ratings_part' + str(i),))
+        openconnection.commit()
+
+    cur.execute('SELECT * FROM output_point_query WHERE Rating = ' + str(ratingValue))
+    sql = "COPY (SELECT * FROM output_point_query) TO STDOUT WITH CSV DELIMITER ','"
+    with open("/home/not-yours/Documents/Assignment1/table.txt", "w") as file:
+        cur.copy_expert(sql, file)
 
 def createDB(dbname='dds_assignment1'):
     """
@@ -129,7 +169,10 @@ ratingsfilepath = '/home/not-yours/Documents/ml-10M100K/' + ratingstablename + '
 # conn = getOpenConnection()
 # cur = conn.cursor()
 #loadRatings(ratingstablename, ratingsfilepath, getOpenConnection())
-rangePartition(ratingstablename,6,getOpenConnection())
+#rangePartition(ratingstablename,6,getOpenConnection())
 # print(cur.execute("SELECT * FROM test;"))
 # print(cur.fetchall())
 #roundRobinPartition(ratingstablename,6,getOpenConnection())
+#roundRobinInsert(ratingstablename,1,364,1,getOpenConnection())
+#rangeQuery(3,4,getOpenConnection(),'/home/not-yours/Documents/ml-10M100K/')
+pointQuery(5,getOpenConnection(),'/home/not-yours/Documents/ml-10M100K/')
